63b16ceaef0b4a8ec94a2fdd7465b3416a6d16b3
Ankit Prakash <anqit.praqash@gmail.com>
2020-12-17 08:30:05 -0700

    rng flatmap

--- exercises/Chapter6/SimpleRNG.scala
+++ exercises/Chapter6/SimpleRNG.scala
@@ -58,11 +58,10 @@ object RNG {
 
         @annotation.tailrec
         def go(c: Int, r: RNG): (List[Int], RNG) = c match {
-            case x if x > 0 => {
+            case x if x > 0 =>
                 val (nextInt, nextRNG) = r.nextInt
                 buf += nextInt
                 go(c - 1, nextRNG)
-            }
             case _ => (buf.toList, r)
         }
 
@@ -71,50 +70,29 @@ object RNG {
 
     val int: Rand[Int] = _.nextInt
     def unit[A](a: A): Rand[A] = rng => (a, rng)
-    def map[A, B](s: Rand[A])(f: A => B): Rand[B] = {
-        rng => {
+    def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
             val (a, rng2) = s(rng)
             (f(a), rng2)
         }
-    }
 
     def doubleViaMap: Rand[Double] =
         map(nonNegativeInt)( _.toDouble / (Int.MaxValue.toDouble + 1))
 
-    def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
-        rng => {
+    def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
             val (a, rng2) = ra(rng)
             val (b, rng3) = rb(rng2)
             (f(a, b), rng3)
         }
-    }
 
     def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
         map2(ra, rb)((_, _))
 
-    def randIntDouble = both(int, double)
+    def randIntDouble: Rand[(Int, Double)] = both(int, double)
 
-    def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
-        val buf = new collection.mutable.ListBuffer[A]
-        rng => {
+    def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = rng => {
             var r = rng
 
-            for (
-                f <- fs
-            ) {
-                val (a, next) = f(r)
-                buf += a
-                r = next
-            }
-            (buf.toList, r)
-        }
-    }
-
-    def sequence_no_buf[A](fs: List[Rand[A]]): Rand[List[A]] = {
-        rng => {
-            var r = rng
-
-            (for (
+            (for(
                 f <- fs
             ) yield {
                 val (a, next) = f(r)
@@ -122,32 +100,52 @@ object RNG {
                 a
             }, r)
         }
-    }
+
 
     def sequence_rec[A](fs: List[Rand[A]]): Rand[List[A]] = {
-        def go(as: List[Rand[A]], r: RNG): (List[A], RNG) = {
+        def go(as: List[Rand[A]], rng: RNG): (List[A], RNG) = {
             as match {
-                case Nil => (List(), r)
-                case a :: tail => {
-                    val (h, next) = a(r)
+                case Nil => (List(), rng)
+                case ra :: tail =>
+                    val (a, next) = ra(rng)
                     val (rest, last) = go(tail, next)
-                    (h :: rest, last)
-                }
+                    (a :: rest, last)
             }
         }
         rng => go(fs, rng)
     }
 
-    def ints_seq(count: Int): Rand[List[Int]] = {
+    def sequence_book[A](fs: List[Rand[A]]): Rand[List[A]] =
+        fs.foldRight(unit(List[A]()))((ra, acc) => map2(ra, acc)(_ :: _))
+
+    def ints_seq(count: Int): Rand[List[Int]] =
         sequence(List.fill(count)(int))
+
+    def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+        val (a, next) = f(rng)
+        g(a)(next)
     }
+
+    def nonNegativeLessThan(n: Int): Rand[Int] = {
+        def retry(i: Int): Boolean = i > Int.MaxValue - Int.MaxValue % n
+
+        flatMap(nonNegativeInt)(i => if(retry(i)) nonNegativeLessThan(n) else unit(i % n))
+    }
+
+    def mapViaFlatMap[A, B](ra: Rand[A])(f: A => B): Rand[B] = {
+        flatMap(ra)(a => unit(f(a)))
+    }
+
+    def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+        flatMap(ra)(a => mapViaFlatMap(rb)(f(a, _)))
+
 }
 
 object Main {
-    def main(args: Array[String]) = {
+    def main(args: Array[String]): Unit = {
         println("ints: " + RNG.ints(5)(SimpleRNG(42))._1)
         println("randIntDouble: " + RNG.randIntDouble(SimpleRNG(42))._1)
-        println("seq: " + RNG.sequence(List(RNG.int, RNG.double _, RNG.double _, RNG.double _))(SimpleRNG(42))._1)
-        println("seq_rec: " + RNG.sequence_rec(List(RNG.int, RNG.double _, RNG.double _, RNG.double _))(SimpleRNG(42))._1)
+        println("seq: " + RNG.sequence(List(RNG.int, RNG.double, RNG.double, RNG.double))(SimpleRNG(42))._1)
+        println("seq_rec: " + RNG.sequence_rec(List(RNG.int, RNG.double, RNG.double, RNG.double))(SimpleRNG(42))._1)
     }
 }
