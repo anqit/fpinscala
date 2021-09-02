f07555a9e9f68695a25001c8eaf16b0b0ffcf748
ankit <anqit.praqash@gmail.com>
2021-09-01 23:36:03 -0600

    through 8.14

--- exercises/Chapter8/Gen.scala
+++ exercises/Chapter8/Gen.scala
@@ -10,6 +10,9 @@ case class Gen[A](sample: State[RNG, A]) {
     def flatMap[B](f: A => Gen[B]): Gen[B] =
         Gen(sample.flatMap { f(_).sample })
 
+    def listOfN(size: Int): Gen[List[A]] =
+        Gen.listOfN(size, this)
+
     def listOfN(size: Gen[Int]): Gen[List[A]] =
         size.flatMap(Gen.listOfN(_, this))
 
@@ -17,10 +20,15 @@ case class Gen[A](sample: State[RNG, A]) {
 }
 
 object Gen {
-    def listOf[A](a: Gen[A]): Gen[List[A]] = ???
+    def listOf[A](a: Gen[A]): SGen[List[A]] =
+        SGen(n => a.listOfN(n))
 
-    def forAll[A](a: Gen[A])(f: A => Boolean, tag: String): Prop =
-        Prop (
+    def listOf1[A](a: Gen[A]): SGen[List[A]] =
+        SGen(n => a.listOfN(n max 1))
+
+
+    def forAll[A](a: Gen[A], tag: String)(f: A => Boolean): Prop =
+        Prop(tag) {
             (max, n, rng) =>
                 randomStream(a)(rng).zip(Stream.from(0)).take(n).map {
                     case (a, i) => try {
@@ -28,19 +36,18 @@ object Gen {
                     } catch {
                         case e: Exception => Falsified(buildMessage(a, e), i, tag)
                     }
-                }.find(_.isFalsified).getOrElse(Passed),
-            tag,
-        )
+                }.find(_.isFalsified).getOrElse(Passed)
+        }
 
-    def forAll[A](a: SGen[A])(f: A => Boolean, tag: String): Prop =
-        forAll(a(_))(f, tag)
+    def forAll[A](a: SGen[A], tag: String)(f: A => Boolean): Prop =
+        forAll(a(_), tag)(f)
 
-    def forAll[A](g: Int => Gen[A])(f: A => Boolean, tag: String): Prop = Prop { (max, n, rng) =>
+    def forAll[A](g: Int => Gen[A], tag: String)(f: A => Boolean): Prop = Prop(tag) { (max, n, rng) =>
         val casesPerSize = (n + max - 1) / max
-        val props: Stream[Prop] = Stream.from(0).take((n min max) + 1).map(i => forAll(g(i))(f, tag))
+        val props: Stream[Prop] = Stream.from(0).take((n min max) + 1).map(i => forAll(g(i), tag)(f))
         val prop: Prop =
             props.map(p =>
-                Prop((max, _, rng) => p.run(max, casesPerSize, rng), tag)
+                Prop(tag) { (max, _, rng) => p.run(max, casesPerSize, rng) }
             ).toList.reduce(_ && _)
 
         prop.run(max, n, rng)
@@ -71,16 +78,15 @@ object Gen {
     def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = {
         val sample: RNG => (List[A], RNG) = rng => {
             var next = rng
-            var list = List[A]()
-            for {
+            var as = for {
                 _ <- 1 to n
-            } {
-                val (a, s) = g.sample.run(next)
+                (a, s: RNG) = g.sample.run(next)
+            } yield {
                 next = s
-                list = a :: list
+                a
             }
 
-            (list, next)
+            (as.toList, next)
         }
 
         Gen(State(sample))
@@ -122,8 +128,8 @@ object SGen {
         Gen.listOfN(n, g)
     }
 
-    def forAll[A](sg: SGen[A])(f: A => Boolean, tag: String): Prop =
-        Gen.forAll(sg(_))(f, tag)
+    def forAll[A](sg: SGen[A], tag: String)(f: A => Boolean): Prop =
+        Gen.forAll(sg(_), tag)(f)
 }
 
 sealed trait Result {
@@ -136,17 +142,17 @@ case class Falsified(failure: FailedCase, successes: SuccessCount, tag: String =
     override def isFalsified: Boolean = true
 }
 
-case class Prop(run: (MaxSize, TestCases, RNG) => Result, tag: String = "") {
-    def &&(p: Prop): Prop = Prop { (maxSize, n, rng) =>
+case class Prop(val tag: String = "")(val run: (MaxSize, TestCases, RNG) => Result) {
+    def &&(p: Prop): Prop = Prop(tag) { (maxSize, n, rng) =>
         run(maxSize, n, rng) match {
             case f: Falsified => f
             case Passed => p.run(maxSize, n, rng)
         }
     }
 
-    def ||(p: Prop): Prop = Prop { (maxSize, n, rng) =>
+    def ||(p: Prop): Prop = Prop(tag) { (maxSize, n, rng) =>
         run(maxSize, n, rng) match {
-            case Falsified(_, _, tag) => (p.copy(tag = tag)).run(maxSize, n, rng)
+            case Falsified(_, _, tag) => (p.copy(tag = tag)(run = p.run)).run(maxSize, n, rng)
             case x => x
         }
     }
@@ -163,8 +169,31 @@ object Prop {
             testCases: TestCases = 100,
             rng: RNG = SimpleRNG(System.currentTimeMillis())): Unit =
         p.run(maxSize, testCases, rng) match {
-            case Falsified(msg, n, tag) => println(s"! falsified prop ${p.tag} after $n passed tests:\n\t/$msg")
+            case Falsified(msg, n, tag) => println(s"! falsified prop ${p.tag} after $n passed tests:\n\t$msg")
             case Passed => println(s"+ OK, prop ${p.tag} passed $testCases tests")
 
         }
+
+    def main(args: Array[String]): Unit = {
+        val smallInt = Gen.choose(-10, 10)
+        val maxProp = Gen.forAll(Gen.listOf1(smallInt), "max test") { ns =>
+          val max = ns.max
+          !ns.exists(_ > max)
+        }
+
+        run(maxProp)
+
+        val sortedProp = Gen.forAll(Gen.unit(List(2, 1, 4, 3)), "sorted test") { ns =>
+          val sorted = ns.sorted
+          sorted == List(1, 2, 3, 4)
+
+          val max = ns.max
+          max == sorted(ns.size - 1)
+
+          val min = ns.min
+          min == sorted(0)
+        }
+
+        run(sortedProp)
+    }
 }
